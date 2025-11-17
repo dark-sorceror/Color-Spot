66c71e5f6cf72928bd75bf8fa857ffa613b2e9ab
Hao Yan <haoyan160@gmail.com>
2025-11-16 22:51:11 -0500

    Add video canvas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useRef } from "react";
+
 import "./App.css";
 
 function App() {
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        async function initCamera() {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: { 
+                    width: 1100, 
+                    height: 700 
+                },
+            });
+
+            if (videoRef.current) {
+                videoRef.current.srcObject = stream;
+            }
+        }
+
+        initCamera();
+    }, []);
+
     return (
-        <>
-			Color Spot
-        </>
+        <div className="wrapper">
+            <h1>Color Spot</h1>
+            <video ref={ videoRef } autoPlay playsInline />
+            <canvas ref={ canvasRef } style={{ display: "none" }} />
+            <div className="color">Color</div>
+        </div>
     );
 }
 
